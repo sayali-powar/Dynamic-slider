eb5af6efbdb7410fca6ba8331f87cf6244ff3974
sayali-powar <144382181+sayali-powar@users.noreply.github.com>
2023-09-13 22:58:13 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,61 @@
+<?php include('sql file.php') ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title>Registration system PHP and MySQL</title>
+
+  <link rel="stylesheet" type="text/css" href="style2.css">
+  
+</head>
+
+<body>
+
+  <div class="header">
+
+  	<h2>Login</h2>
+
+  </div>
+
+	 
+
+  <form method="post" action="login.php">
+
+  	<?php include('error.php'); ?>
+
+  	<div class="input-group">
+
+  		<label>Username</label>
+
+  		<input type="text" name="username" >
+
+  	</div>
+
+  	<div class="input-group">
+
+  		<label>Password</label>
+
+  		<input type="password" name="password">
+
+  	</div>
+
+  	<div class="input-group">
+
+  	<center><button type="submit" class="btn" name="login_user">Login</button></center>
+
+  	</div>
+
+  	<p>
+
+  		Not yet a member? <a href="registration.php">Sign up</a>
+
+  	</p>
+
+  </form>
+
+</body>
+
+</html>
